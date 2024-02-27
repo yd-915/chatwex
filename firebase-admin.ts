9e0682224265c8b3cad86b970343666729fbfd35
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-02-27 12:25:20 -0500

    Update firebase-admin.ts

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -6,7 +6,7 @@ let app
 if (!admin.apps.length) {
   app = admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: "baro-6p3kmc",
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
@@ -15,7 +15,7 @@ if (!admin.apps.length) {
 
 const adminDB = initFirestore({
   credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
+    projectId: "baro-6p3kmc",
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   }),
